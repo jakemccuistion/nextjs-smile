4557ba3c0005b8dc153edd660bcf9e42f899aa41
Jake McCuistion <jake.mccuistion@gmail.com>
2021-04-26 23:06:29 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# Turn That Frown Upside Down
+
+A sample Next.js project that utilizes State Hooks.
+
+Demo: [Click here to view the demo](https://nextjs-smile.vercel.app/)
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+You will need Node to install the software.
+
+### Installing
+
+Download or clone this repo and run 
+```
+npm run dev
+```
+Open [http://localhost:3000](http://localhost:3000) from your browser.
+
+## Built With
+
+* [Next.js](https://nextjs.org/) - The web framework used
+
+## Authors
+
+* **Jake McCuistion** - [JakeMcCuistion.com](https://jakemccuistion.com)
+
+## License
+
+This project is licensed under the MIT License
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,43 @@
 import Head from 'next/head'
+import { useState } from 'react'
+
+const [FROWN_EMOJI, SMILE_EMOJI, BOOM_EMOJI] = ['😕', '🙂', '💥']
 
 export default function Home() {
+  const [currentEmoji, setCurrentEmoji] = useState(FROWN_EMOJI)
+
+  const frown = () => {
+    return setCurrentEmoji(FROWN_EMOJI)
+  }
+  const upsideDown = () => {
+    return setCurrentEmoji(SMILE_EMOJI)
+  }
+  const boom = () => {
+    return setCurrentEmoji(BOOM_EMOJI)
+  }
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Turn that frown upside down!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <section className="card">
+          <h1 className="title">Turn that frown upside down!</h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <h2 onClick={() => upsideDown()} className="emoji">
+            {currentEmoji}
+          </h2>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <p className="buttons">
+            Don't <button onClick={() => frown()}>frown</button> turn it{' '}
+            <button onClick={() => upsideDown()}>upside down!</button>{' '}
+            <button onClick={() => boom()}>Boom!</button>
+          </p>
+        </section>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -78,66 +58,6 @@ export default function Home() {
           align-items: center;
         }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
         .grid {
           display: flex;
           align-items: center;
@@ -153,7 +73,7 @@ export default function Home() {
           flex-basis: 45%;
           padding: 1.5rem;
           text-align: left;
-          color: inherit;
+          color: #333333;
           text-decoration: none;
           border: 1px solid #eaeaea;
           border-radius: 10px;
@@ -163,30 +83,24 @@ export default function Home() {
         .card:hover,
         .card:focus,
         .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+          color: #111111;
+          border-color: #111111;
         }
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
+        .emoji {
+          font-size: 10em;
+          line-height: 0;
         }
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
+        .emoji,
+        button {
+          cursor: pointer;
         }
 
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .title,
+        .emoji,
+        .buttons {
+          text-align: center;
         }
       `}</style>
 
